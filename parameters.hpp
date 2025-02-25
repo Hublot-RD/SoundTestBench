ee5e421590a33c2293cb6353027a4d8fc82bd234
Vincent Desktop <vincent.philippoz@epfl.ch>
2025-02-25 15:40:21 +0100

    Minimal working version (constants to tune)

--- parameters.hpp
+++ parameters.hpp
@@ -9,7 +9,7 @@
 #define SERIAL_BAUD_RATE  115200  // Baud rate for the USB communication
 
 // Motor parameters
-#define A_NOMINAL_SPEED  60000   // Nominal speed of motor A [mRPM]
-#define B_NOMINAL_SPEED  60000   // Nominal speed of motor B [mRPM]
+#define A_NOMINAL_SPEED  40000   // Nominal speed of motor A [mRPM]
+#define B_NOMINAL_SPEED  40000   // Nominal speed of motor B [mRPM]
 
 #endif // PARAMETERS_H
